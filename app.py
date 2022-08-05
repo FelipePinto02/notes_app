b6f78886a9f6c1bb425cc98673d865f75b91760f
Felipe <felipe.pinto.2912@gmail.com>
2022-08-04 21:51:18 -0300

    Started to integrate database with app.py

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-notes = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/notes_app'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Notes(db.Model):
+    __tablename__ = 'notes'
+    id = db.Column(db.Integer, primary_key=True)
+    note = db.Column(db.String(200))
+
+    def __init__(self, note):
+        self.note = note
+
+notes = Notes.query.all()
 
 @app.route("/", methods=["POST", 'GET'])
 def index():
   if request.method == "POST":
     if request.form.get("new"):
       note = request.form.get("note")
-      notes.append(note)
+      data = Notes(note)
+      db.session.add(data)
+      db.session.commit()
+      #notes.append(note)
       return render_template("index.html", notes=notes)
     elif request.form.get("edit"):
-      note = request.form.get("note")
-      notes[int(request.form['edit']) - 1] = note
+      new_note = request.form.get("note")
+      old_note = Notes.query.get(int(request.form['edit']))
+      old_note.note = new_note
+      db.session.commit()
+      #notes[int(request.form['edit']) - 1] = note
       return render_template("index.html", notes=notes)
   else:
     return render_template("index.html", notes=notes)  
