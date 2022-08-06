def0c7228a8467d25a83e622952389a747819c7c
Felipe <felipe.pinto.2912@gmail.com>
2022-08-06 17:58:49 -0300

    Trying to fix database issues

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import os
+import re
+
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 app = Flask(__name__)
 
@@ -10,8 +16,7 @@ if ENV == 'dev':
   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/notes_app'
 else:
   app.debug = False
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:// \
-  mrjjfgmoeqyqwr:598e7b4fb3522ff9e3dcfd2f9f047c03fd22647675d7fcc4619dd5567f5eb3b2@ec2-107-22-122-106.compute-1.amazonaws.com:5432/d9vm3f0nrrepo7'
+  app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
