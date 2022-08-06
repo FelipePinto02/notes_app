6f2b1072ffe7e549e664ae2807953f597db9412b
Felipe <felipe.pinto.2912@gmail.com>
2022-08-06 17:28:02 -0300

    Changed from dev and added Heroku database url

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
   app.debug = True
   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/notes_app'
 else:
   app.debug = False
-  app.config['SQLALCHEMY_DATABASE_URI'] = ''
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:// \
+  mrjjfgmoeqyqwr:598e7b4fb3522ff9e3dcfd2f9f047c03fd22647675d7fcc4619dd5567f5eb3b2@ec2-107-22-122-106.compute-1.amazonaws.com:5432/d9vm3f0nrrepo7'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
