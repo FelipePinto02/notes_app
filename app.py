7f588db18ce55885fa0004ed252e1782727f2ca4
Felipe <felipe.pinto.2912@gmail.com>
2022-08-07 17:12:56 -0300

    Now notes are divided between title and text

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
+from turtle import title
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 import re
 
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
-
 app = Flask(__name__)
 
-ENV = 'prod'
+ENV = 'dev'
 
 if ENV == 'dev':
   app.debug = True
   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/notes_app'
 else:
+  uri = os.getenv("DATABASE_URL")
+  if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
   app.debug = False
   app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
@@ -25,31 +25,39 @@ db = SQLAlchemy(app)
 class Notes(db.Model):
     __tablename__ = 'notes'
     id = db.Column(db.Integer, primary_key=True)
-    note = db.Column(db.String(200))
+    title = db.Column(db.String(200))
+    note = db.Column(db.Text)
 
-    def __init__(self, note):
-        self.note = note
+    def __init__(self, title, note):
+      self.title = title
+      self.note = note
 
 @app.route("/", methods=["POST", 'GET'])
 def index():
   if request.method == "POST":
     if request.form.get("new"):
+      title = request.form.get("title")
       note = request.form.get("note")
-      data = Notes(note)
-      db.session.add(data)
-      db.session.commit()
-      try:
+      if title == '' or note == '':
+        return render_template("add.html", message="Please enter your title and note!", name='new', value='new')
+      else:
+        data = Notes(title, note)
+        db.session.add(data)
+        db.session.commit()
         notes = Notes.query.order_by(Notes.id).all()
         return render_template("index.html", notes=notes)
-      except:
-        return render_template("index.html")
     elif request.form.get("edit"):
+      new_title = request.form.get("title")
       new_note = request.form.get("note")
       old_note = Notes.query.get(int(request.form['edit']))
-      old_note.note = new_note
-      db.session.commit()
-      notes = Notes.query.order_by(Notes.id).all()
-      return render_template("index.html", notes=notes)
+      if new_title == '' or new_note == '':
+        return render_template("add.html", message="Please enter your title and note!", name='edit', value=int(request.form['edit']), old_note=old_note)
+      else:
+        old_note.note = new_note
+        old_note.title = new_title
+        db.session.commit()
+        notes = Notes.query.order_by(Notes.id).all()
+        return render_template("index.html", notes=notes)
   else:
     notes = Notes.query.order_by(Notes.id).all()
     return render_template("index.html", notes=notes)  
@@ -63,7 +71,7 @@ def salvar():
   elif request.form.get("edit"):
     name = 'edit'
     value = int(request.form.get('edit'))
-    old_note = Notes.query.get(value).note
+    old_note = Notes.query.get(value)
     return render_template("add.html", name=name, value=value, old_note=old_note)
   elif request.form.get("delete"):
     value = int(request.form['delete'])
